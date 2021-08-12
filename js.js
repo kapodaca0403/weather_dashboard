5fe65df2e784a98de0666255fbd1e948f7e9cf6b
katerz avocado <ktrinaapodaca@yahoo.com>
2021-08-12 02:31:36 -0600

    trying to put this all together

--- js.js
+++ js.js
@@ -1,6 +1,27 @@
-// need a header that says weather dashboard
+// need a header that says weather dashboard. DONE 
+// need search bar to search for city. DONE 
 // need to link API
 // need to local storage
 // need a container that has the search bar for cities
 // need a containe for the 5 days forecast
 // need a container to add the weather components when clicked on
+$(document).ready(function(){
+
+var city= "";
+$("#citySearch").document("click", function (event){
+    console.log(event);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); 
