7f547d5bac86b4c6a75bba2ee6a921d7678bfdf7
katerz avocado <ktrinaapodaca@yahoo.com>
2021-10-13 23:40:26 -0600

    going to add styling

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-  // var date = moment().format("MM/DD/YYYY");
+  var date = moment().format("MM/DD/YYYY");
   var cityEl = document.querySelector("#cityName")
   var tempEl = document.querySelector("#currWet")
   var humidEl = document.querySelector("#currHum")
   var windEl = document.querySelector("#currWin")
   var UVel = document.querySelector("#currUv")
-  var daytemp = document.querySelector("#dayTemp");
-  var dayhum = document.querySelector("#dayHum");
-  var daywind = document.querySelector("#dayWind");
-  var dayUV = document.querySelector("#dayUv");
+  var container = document.querySelector(".Fiveday-container") //. means class
+  var currentDate = document.querySelector("#currentdate")
+
 
   function fetchWeather(city) {
     var lat  = city.coord.lat ;
     var lon = city.coord.lon;
+    // var forecast = city.daily[""];
   
 
   // city.coord.lat =
@@ -27,6 +27,7 @@ $(document).ready(function () {
 
         //assigning city name to city element as a value
         cityEl.innerText=citName
+      
      
         // date.innerText=citName
         
@@ -35,11 +36,33 @@ $(document).ready(function () {
         humidEl.innerText=data.current.humidity
         windEl.innerText=data.current.wind_speed
         UVel.innerText=data.current.uvi; 
-        daytemp.innerText = data.daily[0].temp; 
-        dayhum.innerText = data.daily.humidity; 
-        daywind.innerText = data.daily.wind_speed; 
-        dayUV.innerText = data.daily.uvi; 
-                
+        currentDate.innerText=date 
+        var dayone = ``;
+        
+        for (let i = 0; i < data.daily.length; i++) {
+          dayone += `<div class="card" id="inputInfo">
+          <h5 class="card-header">5-day Forecast</h5>
+          <div class="card-body">
+          <ul id=inputInfo class="card-text">
+          <p>Temp:${data.daily[i].temp.day}</p>
+          <p id="dayTemp"></p>
+          
+          <p>Wind:${data.daily[i].wind_speed}</p>
+          <p id="dayWind"></p>
+          
+          <p>Humidity:${data.daily[i].humidity}</p>
+          <p id="dayHum"></p>
+          
+          <p>UV:${data.daily[i].uvi}</p>
+          <p id="dayUv"></p>
+          
+          </ul>
+          </div>
+          </div>
+          `;
+        }
+        container.innerHTML=dayone; 
+                curDate(date); 
 
 
 
@@ -50,14 +73,14 @@ $(document).ready(function () {
       });
   }
 
-function formatDate(date) {
-  let d = new Date(date),
-    month = "" + (d.getMonth() + 1),
-    day = "" + d.getDate(),
-    year = "" + d.getFullYear();
+function curDate(date) {
+  let newdate= new Date(date),
+    month = "" + (newdate.getMonth() + 1),
+    day = "" + newdate.getDate(),
+    year = "" + newdate.getFullYear();
   if (month.length < 2) month = "0" + month;
   if (day.length < 2) day = "0" + day;
-  return (formattedDate = year + month + day);
+  return ("year" + "month" + "day");
 }
 
 
@@ -73,10 +96,10 @@ function formatDate(date) {
     }).then(function (response) {
       console.log(response)
       fetchWeather(response.city) 
-      formatDate(); 
-     
-      });
+      
     });
+  });
+  
 
 
 
